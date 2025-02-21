f1d13f0dfb9b7fe74d1d9d0b6605ffcbcb68f243
Lucas Marques <luandrealbuquerque@gmail.com>
2025-02-20 22:53:35 -0300

    add Fastify as a dependency and implement a basic server with routes

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,27 @@
-console.log('ok 2')
+import Fastify from 'fastify';
+
+const fastify = Fastify();
+
+fastify.get('/', async (request, reply) => {
+  return { hello: 'world' };
+})
+
+fastify.route({
+  method: 'GET',
+  url: '/teste',
+  handler: () => {
+    return { hello: 'world' };
+  },
+})
+
+async function start() {
+  try {
+    const host = await fastify.listen({ port: 3000 });
+    console.log(`Server listening on ${host}`);
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+}
+
+start();
